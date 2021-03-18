0215d06fde4b6e0af6c1276f1b14e0a0e3606f4c
DevBadi <ayoubbouchedoub@gmail.com>
2021-03-18 13:41:35 +0100

    Implement the Add and update and delete buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { EmployeeService } from './employee.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   public employees: Employee[];
-  constructor(private employeeService: EmployeeService){}
-  ngOnInit(){
+  constructor(private employeeService: EmployeeService) { }
+  ngOnInit() {
     this.getEmployees();
   }
   public getEmployees(): void {
@@ -24,5 +24,24 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
     );
+  }
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      ;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container.appendChild(button);
+    button.click();
   }
 }
